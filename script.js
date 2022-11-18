ea5fc531edd0fdc363f9b3d56ed70b08b03b3c56
MatthewH18 <90624248+Matthaynes@users.noreply.github.com>
2022-11-18 13:46:13 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,95 @@
+function myFunction() 
+  {
+    var x = document.getElementById("states");
+    var i = x.selectedIndex;
+    var b = x.value;
+    document.getElementById("state-heading").innerHTML = x.options[i].text;
+  document.getElementById("greeting").innerHTML = "Greetings From";
+    document.getElementById("postcard").style.backgroundImage = "url(img/" + b + "@2x.jpg";
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var x = document.getElementById("al").options.length;
+var x = document.getElementById("ak").options.length;
+var x = document.getElementById("az").options.length;
+var x = document.getElementById("ar").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+var x = document.getElementById("mySelect").options.length;
+
+eventlistener
